523ef3acd956c9b19b433cde0fa293adf74a1a3d
Leonardo Afonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2026-03-13 15:50:16 +0000

    Added shooting system

---
+++ Assets/Gun.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+    private float nextTimeToFire = 0f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    InputAction shootAction;
+
+    void Awake()
+    {
+        // Left mouse button binding
+        shootAction = new InputAction("Shoot", binding: "<Mouse>/leftButton");
+    }
+
+    void OnEnable()
+    {
+        shootAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        shootAction.Disable();
+    }
+
+    void Update()
+    {
+        if (shootAction.IsPressed() && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {                
+                target.TakeDamage(damage);
+            }
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
